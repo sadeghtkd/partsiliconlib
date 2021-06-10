f174abc1e2b159a82734cb940a3791357167ac9c
mostafa.hydarzadeh <mostafa.hydarzadeh@gmail.com>
2021-06-10 13:44:50 +0430

    notif review

--- partsiliconlib/src/main/java/com/partsilicon/partsiliconlib/classes/Push.kt
+++ partsiliconlib/src/main/java/com/partsilicon/partsiliconlib/classes/Push.kt
@@ -2,6 +2,8 @@ package com.partsilicon.partsiliconlib.classes
 
 import android.content.Context
 import com.google.firebase.messaging.FirebaseMessaging
+import com.partsilicon.partsiliconlib.BuildConfig
+import com.partsilicon.partsiliconlib.utils.GetVersionCode
 
 class Push{
     /**
@@ -9,5 +11,7 @@ class Push{
      */
     fun subscribeToPush(context: Context){
         FirebaseMessaging.getInstance().subscribeToTopic(context.packageName+"_"+getMarketName(context))
+        FirebaseMessaging.getInstance().unsubscribeFromTopic("v" + (GetVersionCode(context) -1).toString())
+        FirebaseMessaging.getInstance().subscribeToTopic("v" + GetVersionCode(context).toString())
     }
 }
